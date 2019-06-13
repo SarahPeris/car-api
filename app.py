a216e2fc05f7e86960bb0f61ccbd0230a82744ea
SarahPeris <sarah.peris@crem.ch>
2019-06-13 08:20:15 +0200

    MAJ app et models

--- app.py
+++ app.py
@@ -1,9 +1,117 @@
-from flask import Flask, request, jsonify
-from flask_sqlalchemy import SQLAlchemy
-from flask_marshmallow import Marshmallow
-
+#!flask/bin/python
+from flask import Flask, jsonify, abort, make_response, request, url_for
+from models import Car, GasStation
+import json
 app = Flask(__name__)
 
+@app.route('/')
 
-if __name__ = '__main__':
+def index():
+    return "Hello, World"
+
+cars = []
+id_car = 0
+
+stations = []
+id_station = 0
+
+########################### GET ################################
+
+@app.route('/cars', methods=['GET'])
+def get_cars():
+    return jsonify(cars)
+
+@app.route('/stations', methods=['GET'])
+def get_stations():
+    return jsonify({'stations': stations})
+
+
+
+@app.route('/cars/<int:car_id>', methods=['GET'])
+def get_car(car_id):
+    for car in cars:
+        if car.get_id() == car_id:
+            return jsonify({'car': car.car_to_dict()})
+    abort(404)
+
+@app.route('/stations/<int:station_id>', methods=['GET'])
+def get_station(station_id):
+    for station in stations:
+        if station.get_id() == station_id:
+            return jsonify({'station': station.station_to_dict()})
+    abort(404)
+
+
+
+########################### POST ################################
+
+@app.route('/cars', methods=['POST'])
+def create_car():
+    global id_car
+    req = request.json
+    if not req:
+        abort(400)
+    id_car += 1
+    car = Car(id_car, req)
+    cars.append(car)
+    return jsonify({'car': car.car_to_dict()}), 201
+
+
+@app.route('/stations', methods=['POST'])
+def create_station():
+    global id_station
+    req = request.json
+    if not req:
+        abort(400)
+    id_station += 1
+    station = GasStation(id_station, req)
+    stations.append(station)
+    return jsonify({'station': station.station_to_dict()}), 201
+
+########################### PUT ################################
+
+@app.route('/cars/<int:car_id>', methods=['PUT'])
+def update_car(car_id):
+    req = request.json
+    if not req:
+        abort(400)
+    for car in cars:
+        if car.get_id() == car_id:
+            car.set_car(req)
+            return jsonify({'car': car.car_to_dict()})
+    abort(404)
+
+
+@app.route('/stations/<int:station_id>', methods=['PUT'])
+def update_station(station_id):
+    req = request.json
+    if not req:
+        abort(400)
+    for station in stations:
+        if station.get_id() == station_id:
+            station.set_station(req)
+            return jsonify({'station': station.station_to_dict()})
+    abort(404)
+
+
+########################### DELETE ################################
+
+@app.route('/cars/<int:car_id>', methods=['DELETE'])
+def delete_car(car_id):
+    for car in cars:
+        if car.get_id() == car_id:
+            cars.remove(car)
+    return jsonify({'result': True})
+
+@app.route('/stations/<int:station_id>', methods=['DELETE'])
+def delete_station(station_id):
+    for station in stations:
+        if station.get_id() == station_id:
+            stations.remove(station)
+    return jsonify({'result': True})
+
+
+
+
+if __name__ == '__main__':
     app.run(debug=True)
--- models.py
+++ models.py
@@ -1,6 +1,7 @@
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
-from flask.appbuilder import Model
+from flask_appbuilder import Model
+
 
 class GasStation(Model):
     __tablename__ = 'station'
@@ -9,16 +10,54 @@ class GasStation(Model):
     fuel_price = Column(Integer)
     car_observers = relationship("Car", backref="station")
 
-    def __repr__(self):
-        return self.name
+    def __init__(self, id, request):
+        self.id = id
+        self.name = request['name']
+        if 'fuel_price' in request:
+            self.fuel_price = request['fuel_price']
+        if 'car_observers' in request:
+            self.car_observers = request['car_observers']
+
+    def station_to_dict(self):
+        return {'id': self.id, 'name': self.name, 'fuel_price': self.fuel_price, 'car_observers': self.car_observers}
+
+    def set_station(self, request):
+        if 'name' in request:
+            self.name = request['name']
+        if 'fuel_price' in request:
+            self.fuel_price = request['fuel_price']
+        if 'car_observers' in request:
+            self.car_observers = request['car_observers']
+
+    def get_id(self):
+        return self.id
 
 
 class Car(Model):
     __tablename__ = 'car'
     id = Column(Integer, primary_key=True)
     name = Column(String(50), nullable=False)
-    description = Column(string(100))
+    description = Column(String(100))
     station_id = Column(Integer, ForeignKey('station.id'))
 
-    def __repr__(self):
-        return self.name
+    def __init__(self, id, request):
+        self.id = id
+        self.name = request['name']
+        if 'description' in request:
+            self.description = request['description']
+        if 'station_id' in request:
+            self.station_id = request['station_id']
+
+    def car_to_dict(self):
+        return {'id': self.id, 'name': self.name, 'description': self.description, 'station_id': self.station_id}
+
+    def set_car(self, request):
+        if 'name' in request:
+            self.name = request['name']
+        if 'description' in request:
+            self.description = request['description']
+        if 'station_id' in request:
+            self.station_id = request['station_id']
+
+    def get_id(self):
+        return self.id
